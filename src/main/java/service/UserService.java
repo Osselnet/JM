affd463a4a0f1ca3dbdf724abf38a9b2a65be61e
Osselnet <hornets@list.ru>
2020-06-08 12:48:10 +0300

    Перенес Connection в слой DAO.

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -66,31 +66,7 @@ public class UserService {
         }
     }
 
-    private static Connection getMysqlConnection() {
-        try {
-            DriverManager.registerDriver((Driver) Class.forName("com.mysql.jdbc.Driver").newInstance());
-
-            StringBuilder url = new StringBuilder();
-
-            url.
-                    append("jdbc:mysql://").        //db type
-                    append("localhost:").           //host name
-                    append("3306/").                //port
-                    append("db_example?").          //db name
-                    append("user=root&").          //login
-                    append("password=ca5e59f2");       //password
-
-            System.out.println("URL: " + url + "\n");
-
-            Connection connection = DriverManager.getConnection(url.toString());
-            return connection;
-        } catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            e.printStackTrace();
-            throw new IllegalStateException();
-        }
-    }
-
     private static UserDAO getUsertDAO() {
-        return new UserJDBCDAO(getMysqlConnection());
+        return new UserJDBCDAO();
     }
 }
