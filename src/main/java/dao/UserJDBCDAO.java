11c9d1348182c4794291b4aa21436efb4646c631
Osselnet <hornets@list.ru>
2020-06-08 16:08:16 +0300

    Connection вынесен в отдельный файл.

--- src/main/java/dao/UserJDBCDAO.java
+++ src/main/java/dao/UserJDBCDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import model.User;
+import util.DBHelper;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -11,31 +12,7 @@ public class UserJDBCDAO implements UserDAO{
     private Connection connection;
 
     public UserJDBCDAO() {
-        this.connection = getMysqlConnection();
-    }
-
-    private static Connection getMysqlConnection() {
-        try {
-            DriverManager.registerDriver((Driver) Class.forName("com.mysql.jdbc.Driver").newInstance());
-
-            StringBuilder url = new StringBuilder();
-
-            url.
-                    append("jdbc:mysql://").        //db type
-                    append("localhost:").           //host name
-                    append("3306/").                //port
-                    append("db_example?").          //db name
-                    append("user=root&").          //login
-                    append("password=ca5e59f2");       //password
-
-            System.out.println("URL: " + url + "\n");
-
-            Connection connection = DriverManager.getConnection(url.toString());
-            return connection;
-        } catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            e.printStackTrace();
-            throw new IllegalStateException();
-        }
+        this.connection = DBHelper.getMysqlConnection();
     }
 
     @Override
