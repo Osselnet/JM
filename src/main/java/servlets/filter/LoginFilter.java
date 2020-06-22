509cb72eef7aa47f0196556fec6a861ffaf8a64c
Osselnet <hornets@list.ru>
2020-06-22 16:32:39 +0300

    Сделал фильтры, которые пропускают только зарегистрированных пользователей, в зависимости от роли

--- src/main/java/servlets/filter/LoginFilter.java
+++ src/main/java/servlets/filter/LoginFilter.java
@@ -21,18 +21,22 @@ public class LoginFilter implements Filter {
         HttpSession session = req.getSession();
         boolean isAuth = session.getAttribute("userObject") != null;
 
+        boolean isLogout = req.getRequestURI().equals("/logout");
+
         User user;
         boolean isUserRole = false;
         boolean isAdminRole = false;
-         if (isAuth) {
+        if (isAuth) {
             user = (User) session.getAttribute("userObject");
             isUserRole = user.getRole().equals("user");
             isAdminRole = user.getRole().equals("admin");
         }
 
-        if (isAdminRole) {
+        if (isLogout) {
+            servletRequest.getRequestDispatcher("/logout").forward(servletRequest, servletResponse);
+        } else if (isAdminRole) {
             res.sendRedirect(req.getContextPath() + "/admin");
-         } else if (isUserRole) {
+        } else if (isUserRole) {
             servletRequest.getRequestDispatcher("/user").forward(servletRequest, servletResponse);
         } else {
             filterChain.doFilter(servletRequest, servletResponse);
